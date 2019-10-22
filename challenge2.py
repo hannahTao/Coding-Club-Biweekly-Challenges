7a548c9407a3f2dea4f6e6a6c953972a7481d366
hannahTao <56075416+hannahTao@users.noreply.github.com>
2019-10-22 14:16:33 -0400

    Update challenge2.py

--- challenge2.py
+++ challenge2.py
@@ -2,10 +2,10 @@ def square_difference(n):
     sumOfSquares = 0
     sumOfIntegers = 0
     for i in range (n+1):       # find sum of squares of integers
-        sumOfSquares += n**2
+        sumOfSquares += i**2
     for j in range (n+1):       # find square of sum of integers
-        sumOfIntegers += n
+        sumOfIntegers += j
     return sumOfIntegers**2 - sumOfSquares  # calculate difference
     
-square_difference(10)          # example should equal 2640
-square_difference(100)         # answer 
+print(square_difference(10))     # example should equal 2640
+print(square_difference(100))    # answer 
